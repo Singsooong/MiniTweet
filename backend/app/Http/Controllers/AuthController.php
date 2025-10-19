4fbae507b634b5cb478c9edbbe8346a300771a38
iamic.dev <dequinacarlo761@gmail.com>
2025-10-20 01:01:00 +0800

    feat: implement AuthController with Register

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required|string',
+            'surname' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()]);
+        }
+        $user = User::create([
+            'firstname' => $request['firstname'],
+            'surname' => $request['surname'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password'])
+        ]);
+        $fields ['firstname'] = $user -> firstname;
+        $fields ['surname'] = $user -> surname;
+        $fields ['email'] = $user -> email;
+        $fields ['token'] = $user->createToken('App')->plainTextToken;
+
+
+        return response()->json($fields);
+
+    }
+}
