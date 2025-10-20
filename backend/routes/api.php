f29f6edbd273ac360454932d13072e06f25d630c
iamic.dev <dequinacarlo761@gmail.com>
2025-10-20 14:29:02 +0800

    feat: created Tweet controller and Tweet Table

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TweetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+   Route::resource('tweets', TweetController::class);
+});
 
